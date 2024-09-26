d7777256248680fe9c18d08fe7648e039becc4af
DmytroWW <veksbrightside@gmail.com>
2024-09-26 14:20:59 +0300

    feat: add scripts

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const cards = document.querySelectorAll('.card')
 console.log(cards)
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-	this.classList.toggle('flip')
+	this.classList.add('flip');
+
+	if(!hasFlippedCard) {
+		hasFlippedCard = true;
+		firstCard = this;
+		return;
+	}
+
+	secondCard = this;
+	hasFlippedCard = false;
+
+	checkForMatch();
+}
+
+function checkForMatch() {
+	if (firstCard.dataset.type === secondCard.dataset.type) {
+		disableCards();
+		return;
+	}
+
+	unflipCards();
+}
+
+function disableCards () {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+	setTimeout(() => {
+		firstCard.classList.remove('flip')
+		secondCard.classList.remove('flip')
+	}, 1500);
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard))
